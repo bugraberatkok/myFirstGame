2cb8fa6c14a7ed88cb59d64eb24ef617281c736d
Buğra Berat Kök <bugraberat1@hotmail.com>
2024-12-13 17:17:30 +0300

    Initial commit

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+
+public class Credits : MonoBehaviour
+{
+   public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+   public void StartGame()
+    {
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+}
